c2050afea4eb8d096fbeb5bfc46b0d7273f01397
badboydeuce <badboydeuce@bill.com>
2026-05-06 17:23:53 -0700

    Update __init__.py

--- bot/keyboards/__init__.py
+++ bot/keyboards/__init__.py
@@ -1,1 +1,38 @@
 # Inline keyboards
+"""
+Keyboards module initialization
+"""
+
+from .main_menu import get_main_menu_keyboard
+from .inline_keyboards import (
+    get_back_keyboard,
+    get_confirmation_keyboard,
+    get_country_keyboard,
+    get_service_keyboard,
+    get_rental_duration_keyboard,
+    get_order_action_keyboard,
+    get_orders_list_keyboard,
+    get_payment_keyboard,
+    get_transaction_history_keyboard,
+    get_wallet_keyboard,
+    get_buy_type_keyboard,
+    get_main_menu_inline,
+    get_support_keyboard
+)
+
+__all__ = [
+    'get_main_menu_keyboard',
+    'get_back_keyboard',
+    'get_confirmation_keyboard',
+    'get_country_keyboard',
+    'get_service_keyboard',
+    'get_rental_duration_keyboard',
+    'get_order_action_keyboard',
+    'get_orders_list_keyboard',
+    'get_payment_keyboard',
+    'get_transaction_history_keyboard',
+    'get_wallet_keyboard',
+    'get_buy_type_keyboard',
+    'get_main_menu_inline',
+    'get_support_keyboard'
+]
