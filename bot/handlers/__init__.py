c8649b3da831743a60fad3ffc829423cd444e8be
badboydeuce <badboydeuce@bill.com>
2026-05-06 17:26:00 -0700

    Update __init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,7 @@
+"""
+Handlers module initialization
+"""
+
+from . import start, wallet, activation, rental, orders, admin
+
+__all__ = ['start', 'wallet', 'activation', 'rental', 'orders', 'admin']
