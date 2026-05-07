b639dd03467e5f417ac809ae9b86d7dcf91182d3
badboydeuce <badboydeuce@bill.com>
2026-05-06 17:41:05 -0700

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -2,6 +2,18 @@
 Handlers module initialization
 """
 
-from . import start, wallet, activation, rental, orders, admin
+from .start import router as start_router
+from .wallet import router as wallet_router
+from .activation import router as activation_router
+from .rental import router as rental_router
+from .orders import router as orders_router
+from .admin import router as admin_router
 
-__all__ = ['start', 'wallet', 'activation', 'rental', 'orders', 'admin']
+__all__ = [
+    'start_router',
+    'wallet_router', 
+    'activation_router',
+    'rental_router',
+    'orders_router',
+    'admin_router'
+]
